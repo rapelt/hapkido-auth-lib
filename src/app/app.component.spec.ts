f6e5e0296672ac8b32fc9ddb636834fc9fee35db
Rebekah Higgins <rebekahapelt@gmail.com>
2019-12-04 14:41:11 +1000

    Inital deploy with travis

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AuthLibModule } from 'auth-lib';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AuthLibModule
       ],
       declarations: [
         AppComponent
