9598a18999b4af3dccc619085f945aa881463d1f
Rebekah Higgins <rebekahapelt@gmail.com>
2019-12-06 09:58:51 +1000

    Fixed broken tests

--- projects/auth-lib/src/lib/auth-lib.component.spec.ts
+++ projects/auth-lib/src/lib/auth-lib.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthLibComponent } from './auth-lib.component';
+import {IonicModule} from '@ionic/angular';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {SignInComponent} from './sign-in/sign-in.component';
 
 describe('AuthLibComponent', () => {
   let component: AuthLibComponent;
@@ -8,7 +11,12 @@ describe('AuthLibComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthLibComponent ]
+      declarations: [ AuthLibComponent, SignInComponent ],
+      imports: [
+        IonicModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
--- projects/auth-lib/src/lib/sign-in/sign-in.component.spec.ts
+++ projects/auth-lib/src/lib/sign-in/sign-in.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInComponent } from './sign-in.component';
+import {IonicModule} from '@ionic/angular';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
@@ -8,7 +10,12 @@ describe('SignInComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      declarations: [ SignInComponent ],
+      imports: [
+        IonicModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
